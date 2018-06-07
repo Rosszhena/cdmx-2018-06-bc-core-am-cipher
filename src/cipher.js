9d22bea17ab209c369bf479a70bce9bd4d843747
Maria Rosa <ross.zhena@gmail.com>
2018-06-07 11:09:38 -0500

    primera funcionalidad de cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,20 @@
-window.cipher = {
+//window.cipher = {
   // ...
-};
+
+//};
+
+let  cadena = prompt("indica la cadena a cifrar");
+let iteraciones = parseInt(prompt("indica el numero de iteraciones"));
+
+
+  for(let i=0; i < cadena.length; i++){
+    let letra = cadena.charCodeAt(i); //Obtiene el codigo ASCII de la letra en la posicion q se le indica
+    let codigo = (letra - 65 + iteraciones)% 26 + 65;
+    console.log("CON FORMULA" + codigo);
+    console.log("ASCII"+letra);
+    //let mensajecodigo+=codigo;
+    let cifrado=String.fromCharCode(codigo);
+    console.log("cifrdo" + cifrado);
+    document.write(cifrado);
+    //return cifrado;
+}   //document.write(letra);

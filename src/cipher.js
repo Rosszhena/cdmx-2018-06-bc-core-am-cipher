169c6f22961acb9846290792e2fafd71d56f4f6c
Maria Rosa <ross.zhena@gmail.com>
2018-06-07 22:49:05 -0500

    -se agrego descifrado y funciones

--- src/cipher.js
+++ src/cipher.js
@@ -6,15 +6,23 @@
 let  cadena = prompt("indica la cadena a cifrar");
 let iteraciones = parseInt(prompt("indica el numero de iteraciones"));
 
-
+const cipher  = function(cadena, iteraciones){
   for(let i=0; i < cadena.length; i++){
-    let letra = cadena.charCodeAt(i); //Obtiene el codigo ASCII de la letra en la posicion q se le indica
-    let codigo = (letra - 65 + iteraciones)% 26 + 65;
-    console.log("CON FORMULA" + codigo);
-    console.log("ASCII"+letra);
-    //let mensajecodigo+=codigo;
-    let cifrado=String.fromCharCode(codigo);
-    console.log("cifrdo" + cifrado);
+    let letra = cadena.toUpperCase().charCodeAt(i); //Obtiene el codigo ASCII de la letra en la posicion q se le indica
+    let cifrado=String.fromCharCode((letra - 65 + iteraciones)% 26 + 65);
     document.write(cifrado);
-    //return cifrado;
-}   //document.write(letra);
+    console.log(cifrado);
+}   //llave for
+}
+
+const desencriptar=function(cadena, iteraciones){
+  for(let i=0; i < cadena.length; i++){
+    let letra = cadena.toUpperCase().charCodeAt(i); //Obtiene el codigo ASCII de la letra en la posicion q se le indica
+    let descifrado=String.fromCharCode((letra + 65 - iteraciones)% 26 + 65);
+    console.log("descifrdo" + descifrado);
+    document.write( descifrado);
+}
+};
+
+cipher(cadena, iteraciones);
+desencriptar(cadena, iteraciones);

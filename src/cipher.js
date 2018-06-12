a7b00b5d331970cee49bf5851e1325c9c162fa81
Maria Rosa <ross.zhena@gmail.com>
2018-06-12 10:28:28 -0500

    se agrego espacio a la cadena de salida

--- src/cipher.js
+++ src/cipher.js
@@ -2,28 +2,36 @@
   // ...
 
 //};
-
-const cipher  = function(){
+const cipher  = () => {
   let cadena = document.getElementById("cadena").value;
   let iteraciones = parseInt(document.getElementById("iteraciones").value);
-  let codAsc;
   let cifrado="";
   for(let i=0; i < cadena.length; i++){
-    codAsc = cadena.toUpperCase().charCodeAt(i); //Obtiene el codigo ASCII de la letra en la posicion q se le indica
-    cifrado+=String.fromCharCode((codAsc - 65 + iteraciones)% 26 + 65);
+    let codAsc = cadena[i].toUpperCase().charCodeAt(); //Obtiene el codigo ASCII de la letra en la posicion q se le indica
+
+    if(codAsc == 32 || ){
+      cifrado += cadena[i];
+      console.log(codAsc);
+    } else {
+      let formula =(cadena[i].toUpperCase().charCodeAt() - 65 + iteraciones)% 26 + 65;
+      console.log(formula);
+      cifrado +=String.fromCharCode(formula);
+    }
+
 
 }  //llave for
 document.getElementById("resultado").value=cifrado;
+return cifrado;
 }
-const desencriptar=function(){
-  let cadena = document.getElementById("cadena").value;
-  let iteraciones = parseInt(document.getElementById("iteraciones").value);
-  let codAsc;
-  let descifrado="";
-  for(let i=0; i < cadena.length; i++){
-    codAsc = cadena.toUpperCase().charCodeAt(i); //Obtiene el codigo ASCII de la letra en la posicion q se le indica
-    descifrado+=String.fromCharCode((codAsc + 65 - iteraciones)% 26 + 65);
-
-}
-document.getElementById("resultado").value=descifrado;
-}
+// const desencriptar= () =>{
+//   let cadena = document.getElementById("cadena").value;
+//   let iteraciones = parseInt(document.getElementById("iteraciones").value);
+//   let codAsc;
+//   let descifrado="";
+//   for(let i=0; i < cadena.length; i++){
+//     codAsc = cadena.toUpperCase().charCodeAt(i); //Obtiene el codigo ASCII de la letra en la posicion q se le indica
+//     descifrado+=String.fromCharCode((codAsc + 65 - iteraciones)% 26 + 65);
+//
+// }
+// document.getElementById("resultado").value=descifrado;
+// }
